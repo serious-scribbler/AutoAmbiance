7c59ca0ce5b888c59906458c2838833b7fa939d9
Phil Niehus <contact@pniehus.de>
2025-09-11 07:36:18 +0200

    🔧 Added utils for downloading models

--- src/composables/ai/usePipeline.ts
+++ src/composables/ai/usePipeline.ts
@@ -1,5 +1,10 @@
 import { ref } from 'vue';
-import { pipeline, Pipeline, type PipelineType, type PretrainedModelOptions, type AllTasks } from '@huggingface/transformers';
+import { pipeline, Pipeline, type PipelineType, type PretrainedModelOptions, type AllTasks, env } from '@huggingface/transformers';
+
+env.localModelPath = "/models/";
+env.allowLocalModels = true;
+env.allowRemoteModels = false;
+
 
 export function usePipeline<T extends PipelineType>(pipelineType: T, model: string | undefined = undefined, options: PretrainedModelOptions = {}) {
     const result = ref<AllTasks[T] | undefined>(undefined);
--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     },
   },
   server: {
+    origin: "*",
     cors: true
   },
   preview: {
